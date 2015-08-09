eb6ace33d6a71bf4b3f1aec91b484c2b06cf6cc9
Michael Huang <fernozzle@htwins.net>
2015-08-09 12:41:08 -0700

    Add PBR shading

--- _image.glsl
+++ _image.glsl
@@ -1,3 +1,4 @@
+#define PI 3.141592
 #define TWO_PI 6.2831853
 #define VERTICAL_FOV 40.
 #define MAX_ALPHA .9
@@ -21,30 +22,30 @@ float sphere_map(vec3 p, vec3 center, float radius) {
 //   Index [0, 1) = smoothness; RGB = albedo
 float map(in vec3 p, out vec4 material) {
     float dist = box_map(p, vec3(0.), vec3(1.), 0.5);
-    material = vec4(0.4, 0.7, 0.9, 0.);
+    material = vec4(0.4, 0.7, 0.9, 0.5);
     
     float new_dist = sphere_map(p, vec3(sin(iGlobalTime * 5.) * .8 + 2., 0., 0.), 1.);
     if (new_dist < dist) {
         dist = new_dist;
-        material = vec4(0.8, 0.1, 0.1, 0.);
+        material = vec4(0.8, 0.1, 0.1, 0.5);
     }
     
     new_dist = sphere_map(p, vec3(0., sin(iGlobalTime * 4.) * 1. + 3., 0.), .5);
     if (new_dist < dist) {
         dist = new_dist;
-        material = vec4(0.9, 0.9, 0.3, 0.);
+        material = vec4(0.9, 0.9, 0.3, 0.5);
     }
     
     new_dist = box_map(p, vec3(0.5, 0.1, -1.), vec3(0.4, 0., 1.), 0.2);
     if (new_dist < dist) {
         dist = new_dist;
-        material = vec4(1., 1., 1., 0.);
+        material = vec4(1., 1., 1., 0.3);
     }
     
     new_dist = box_map(p, vec3(-2.5, 0., 0.), vec3(.2, .2, .2), 0.4);
     if (new_dist < dist) {
         dist = new_dist;
-        material = vec4(1., 1., 1., 0.);
+        material = vec4(1., .5, .2, 0.5);
     }
     
     return dist;
@@ -91,6 +92,42 @@ float ao(vec3 p, vec3 normal, float coc) {
     return pow(brightness, .2);
 }
 
+vec3 shade_standard(vec3 albedo, float roughness, vec3 normal, vec3 light_dir, vec3 ray_dir) {
+    
+    float F0 = .1;
+    float diffuse_specular_mix = .3;
+    
+    float nl = dot(normal, light_dir);
+    float nv = dot(normal, -ray_dir);
+    if (nl > 0. && nv > 0.)
+    {
+        vec3 haf = normalize(light_dir - ray_dir);
+        float nh = dot(normal, haf); 
+        float vh = dot(-ray_dir, haf);
+        
+        vec3 diffuse = albedo*nl;
+        
+        // Cook-Torrance
+        float a = roughness * roughness;
+        float a2 = a * a;
+        float dn = nh * nh * (a2 - 1.) + 1.;
+        float D = a2 / (PI * dn * dn);
+        
+        float k = pow(roughness + 1., 2.0) / 8.;
+        float nvc = max(nv, 0.);
+        float g1v = nvc / (nvc * (1. - k) + k);
+        float g1l = nl  / (nl  * (1. - k) + k);
+        float G = g1l * g1v;
+
+        float F = F0 + (1. - F0) * exp2((-5.55473 * vh - 6.98316) * vh);
+        
+        float specular = (D * F * G) / (4. * nl * nv);
+    	
+        return mix(vec3(specular), diffuse, diffuse_specular_mix);
+    }
+    return vec3(0.);
+}
+
 float cocSize(float dist) {
     //return 1. / iResolution.y * dist;
     return (sin(iGlobalTime * 3.) * 4. + 5.) / iResolution.y * dist;
@@ -132,13 +169,13 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
                 float alpha = toward_camera * coc_kernel(coc, map_dist);
                 vec3 surface_color = vec3(0.);
                 
-                vec3 light_direction = normalize(vec3(-0.4, 1., -0.3));
-                float light_intensity;
-                light_intensity = max(dot(normal, light_direction), 0.) * soft_shadow(point, light_direction, .2, coc, .1);
+                vec3 light_dir = normalize(vec3(-0.4, 1., -0.3));
+                vec3 light_intensity;
+                light_intensity = shade_standard(mat.rgb, mat.a, normal, light_dir, ray_dir)/* * soft_shadow(point, light_dir, .2, coc, .1)*/;
                 
-                surface_color += mat.rgb * (.01 + .99 * light_intensity) * vec3(1., 0.95, 0.7);
+                surface_color += light_intensity * vec3(1., 0.95, 0.7);
                 
-                surface_color += mat.rgb * ao(point, normal, coc) * vec3(0.2, 0.8, 1.) * .2;
+                //surface_color += mat.rgb * ao(point, normal, coc) * vec3(0.2, 0.8, 1.) * .2;
                 
                 // "Alpha-under"ing surface_color/alpha beneath col
                 float added_coverage = alpha * (1. - col.a);
@@ -150,7 +187,7 @@ void mainImage(out vec4 fragColor, in vec2 fragCoord) {
         }
         
         iters++;
-        ray_len += max(map_dist - .5 * coc, .5 * coc);
+        ray_len += max(map_dist - .5 * coc, .3 * coc);
     }
     
     col = vec4(sqrt(mix(bg, col.rgb, min(col.a / MAX_ALPHA, 1.))), 1.);
